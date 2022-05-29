73dbad229b7d4daf5879b6731934819490c8f357
swaroop-30 <swaroopchintad30@gmail.com>
2022-05-29 13:42:01 +0530

    make me better

--- main.py
+++ main.py
@@ -6,20 +6,17 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 from selenium.webdriver.support.select import Select
 
-def automate():
-
-    time.sleep(1)
-
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--no-sandbox")
 
 
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
-    #initialize chrome webdriver
+driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options= chrome_options)
+# initialize chrome webdriver
 
+time.sleep(1)
 # Open the e-commerce URL and maximize the window
 driver.get('http://tutorialsninja.com/demo/')
 driver.maximize_window()
